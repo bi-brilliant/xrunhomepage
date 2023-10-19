9077be2b879702319e8ea39e0a30de850b2afe34
Bilal Jibril <bilaljibrilliant@gmail.com>
2023-10-19 18:14:12 +0700

    Merger

--- connection.php
+++ connection.php
@@ -1,11 +1,182 @@
-<?php 
-    $connection = mysqli_connect("db-main-master.ctauiqqlg2bt.ap-southeast-1.rds.amazonaws.com", "xrundb", "xrundatA6a52!!" ,"xrun"); 
-    $sql = mysqli_query($connection, "SELECT * FROM homepagenotice"); 
-    $result = array(); 
-     
-    while ($row = mysqli_fetch_assoc($sql)) { 
-        $data[] = $row; 
-    } 
- 
-    echo json_encode(array("result" => $data)); 
-?>
+<?php
+
+// $connection = mysqli_connect("db-main-master.ctauiqqlg2bt.ap-southeast-1.rds.amazonaws.com", "xrundb", "xrundatA6a52!!", "xrun");
+$connection = mysqli_connect("localhost", "root", "", "tempxscan", 3307);
+$xrunConn = mysqli_connect("localhost", "root", "", "xrun", 3307);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$action = $_REQUEST["action"];
+if (!$action) {
+    echo 'illegal access';
+    return;
+}
+
+header('Content-Type: application/json');
+
+switch ($action) {
+    // Get Coin Market Cap Data
+    case 'getCMCData':
+        $query = mysqli_query($connection, "SELECT * FROM `cmcdata`");
+
+        while($data = mysqli_fetch_assoc($query)) {
+            $result[] = $data;
+        }
+
+        echo json_encode($result);
+        break;
+
+        // Get Yesterday Price of Coin
+    case 'getYesterdayData':
+        $query = mysqli_query($connection, "SELECT * 
+                                            FROM `cmcdata` 
+                                            WHERE DATE_FORMAT(`timestamp`, '%Y-%m-%d') = DATE_SUB(CURDATE(), INTERVAL 1 DAY);");
+
+        while($data = mysqli_fetch_assoc($query)) {
+            $result[] = $data;
+        }
+
+        echo json_encode($result);
+        break;
+
+        // Get Disclosure Data
+    case 'getDisclosureData':
+        $query = mysqli_query($xrunConn, "SELECT * FROM `disclosure`");
+
+        while($data = mysqli_fetch_assoc($query)) {
+            $result[] = $data;
+        }
+
+        echo json_encode($result);
+        break;
+
+
+        // ----- Sign Up
+    case 'signup':
+        $username = htmlspecialchars($_GET["username"], ENT_QUOTES, 'UTF-8');
+        $email = htmlspecialchars($_GET["email"], ENT_QUOTES, 'UTF-8');
+        $password = htmlspecialchars($_GET["password"], ENT_QUOTES, 'UTF-8');
+        $region = $_GET["codeRegion"];
+        $country = $_GET["country"];
+        $countryCode = $_GET['countryCode'];
+        $mobile = $_GET["mobile"];
+        $gender = $_GET["gender"];
+        $age = $_GET["age"];
+        $encryptPassword = password_hash($password, PASSWORD_DEFAULT);
+        $nickname = htmlspecialchars($_GET["nickname"], ENT_QUOTES, 'UTF-8');
+
+        $query = mysqli_query($conn, "INSERT INTO `members` 
+                                                    (`member`, `uuid`, `type`, 
+                                                     `email`, `pin`, 
+                                                     `username`, `nickname`, `mobilecode`, 
+                                                     `mobile`, `ages`, 
+                                                     `gender`, `country`, 
+                                                     `countrycode`, `region`, 
+                                                     `status`, `datechanged`, 
+                                                     `wallet`, `datejoin`, 
+                                                     `datepinchanged`, `isLockable`) 
+                                            VALUES (NULL, '', '1001', 
+                                                    '$email', '$encryptPassword', 
+                                                    '$username', '$nickname', '$country', 
+                                                    '$mobile', '$age',
+                                                    '$gender', $country, 
+                                                    '$countryCode', $region, 
+                                                    '1001', current_timestamp(), 
+                                                    '0', current_timestamp(), 
+                                                    current_timestamp(), b'0'
+                                                    )");
+
+        if ($query) {
+            $newID = mysqli_insert_id($conn);
+
+            $hash = md5($newID);
+            $uuid = substr($hash, 0, 6);
+
+            $addUUID = mysqli_query($conn, "UPDATE `members` SET `uuid` = '$uuid' WHERE `members`.`member` = $newID;");
+
+            if ($addUUID) {
+                $hairInventory = $gender == 1101 ? '1,2,3,4,5,6' : '1,2,3,4,5,6,7,8,9,10,11';
+                // Insert Character Setting for New Member with Default Data
+                $createCharacter = mysqli_query($conn, "INSERT INTO `character` (`char_ID`, `member`, 
+                                                                                 `head_face`, `head_eye`, 
+                                                                                 `head_ear`, `head_mouth`, 
+                                                                                 `head_nose`, `hair_style`,
+                                                                                 `hair_inventory`, 
+                                                                                 `body_height`, `body_fat`, 
+                                                                                 `skin_color`, `skin_saturation`, 
+                                                                                 `skin_hue`, `clothes_inventory`, 
+                                                                                 `clothes_current`, `datecreate`, 
+                                                                                 `dateupdate`) 
+                                                                        VALUES (NULL, '$newID', 
+                                                                                '0.0000000000000000000000000', '0.0000000000000000000000000', 
+                                                                                '0.0000000000000000000000000', '0.0000000000000000000000000', 
+                                                                                '0.0000000000000000000000000', '1', 
+                                                                                '$hairInventory',
+                                                                                '1.0000000000000000000000000', '1.0000000000000000000000000', 
+                                                                                '(R=1.000000,G=1.000000,B=1.000000,A=1.000000)', '0.0000000000000000000000000', 
+                                                                                '1.0000000000000000000000000', '4', 
+                                                                                '4', current_timestamp(), 
+                                                                                current_timestamp());");
+
+
+                if ($createCharacter) {
+                    $createWallet = mysqli_query($conn, "INSERT INTO `wallet` (`wallet`, `member`, 
+                                                                               `point`, `coin`, 
+                                                                               `currency`, `timestamp`, 
+                                                                               `timeUpdate`) 
+                                                                       VALUES (NULL, '$newID', 
+                                                                               '25000', '0', 
+                                                                               '1', NOW(), 
+                                                                               NOW())");
+
+                    if($createWallet) {
+                        $response = array(
+                            "status" => 1,
+                            "message" => "Success",
+                            "data" => [
+                                [
+                                    "statusDesc" => "success"
+                                ]
+                            ]
+                        );
+                    } else {
+                        $response = array(
+                            "status" => 1,
+                            "message" => "Character Failed Insert",
+                            "data" => [
+                                [
+                                    "statusDesc" => "failInsert"
+                                ]
+                            ]
+                        );
+                    }
+                } else {
+                    $response = array(
+                        "status" => 1,
+                        "message" => "Character Failed Insert",
+                        "data" => [
+                            [
+                                "statusDesc" => "failInsert"
+                            ]
+                        ]
+                    );
+                }
+            }
+        } else {
+            $response = array(
+                "status" => 0,
+                "message" => "Fail",
+                "data" => [
+                    [
+                        "statusDesc" => "fail"
+                    ]
+                ]
+            );
+        }
+
+        echo json_encode($response);
+        break;
+
+}
